a049869601540a37974b9226a1b5d44342691be5
luis <luisfr364@gmail.com>
2026-05-19 01:12:21 -0300

    refactor: Added some TODO comments; Improved the formatting of the code with formatters.

--- controllers/support_page_controller.py
+++ controllers/support_page_controller.py
@@ -1,6 +1,7 @@
 import slint
-import threading
+
 from helpers.support_scripts_helper import SupportScriptsHelper
+
 
 class SupportPageController:
     def __init__(self, app):
@@ -10,30 +11,29 @@ class SupportPageController:
     # Used on the callback of the script execution to show the output of the script in the console
     @slint.callback
     def update_console_output(self, new_message):
-        message = self.app.console_output;
-
-
+        message = self.app.console_output
         self.app.console_output = message + "\n" + new_message
-
-
 
     @slint.callback
     def on_script_module_click(self, name, description):
         print(f"Script selecionado: {name} - {description}")
-        self.app.selected_script = {"name": name, 
-                                    "description": description, 
-                                    "name_short": name[:24] + "..." if len(name) > 24 else name
-                                    }  # Example: truncate name to first 24 characters
-        
-    
+        self.app.selected_script = {
+            "name": name,
+            "description": description,
+            "name_short": name[:24] + "..." if len(name) > 24 else name,
+        }  # Example: truncate name to first 24 characters
+
     # Call the scripts helper to run the scripts based on the selected_script var
     @slint.callback
     async def on_run_script(self):
         helper = SupportScriptsHelper(self.app)
-        selected_script = self.app.selected_script # Gets the selected_script var value from the main window
+        selected_script = (
+            self.app.selected_script
+        )  # Gets the selected_script var value from the main window
 
-
-        match selected_script.name: # Decide which script to run based on the selected script
+        match (
+            selected_script.name
+        ):  # Decide which script to run based on the selected script
             case "Limpeza de Arquivos Temporários":
                 await helper.run_clean_temp_files_script(self.update_console_output)
             case "Correção de erros de internet":
--- helpers/support_scripts_helper.py
+++ helpers/support_scripts_helper.py
@@ -1,26 +1,30 @@
-import os
-import time
-import subprocess
 import asyncio
+import os
+import subprocess
+import time
+
 
 class SupportScriptsHelper:
     def __init__(self, app):
         self.app = app
 
+    # TODO: Check if there's no danger of erasing important files
     async def run_clean_temp_files_script(self, cb):
-        temp_dir = os.environ.get('TEMP', '/tmp')  # Get the temp directory from environment variables
-
-
+        temp_dir = os.environ.get(
+            "TEMP", "/tmp"
+        )  # Get the temp directory from environment variables
 
         if not temp_dir or not os.path.exists(temp_dir):
             cb("Diretório temporário não encontrado.")
             return
-        
+
         try:
             # List all files in the temp directory
             files = os.listdir(temp_dir)
             deleted_files_count = 0
-            cb(f"Foram encontrados {len(files)} arquivos temporários. Iniciando limpeza...")
+            cb(
+                f"Foram encontrados {len(files)} arquivos temporários. Iniciando limpeza..."
+            )
             await asyncio.sleep(1)  # Simulate some processing time
 
             for file in files:
@@ -35,39 +39,40 @@ class SupportScriptsHelper:
             cb(f"{deleted_files_count} arquivos temporários deletados.")
         except Exception as e:
             cb(f"Erro ao acessar diretório temporário: {e}")
-    
-    async def run_fix_internet_errors_script(self, cb):   
-           await asyncio.sleep(1) # Simulate some processing time
-           cb("Iniciando correção de erros de internet...")
 
-           commands = [
-             {'command': 'ipconfig /release', 'description': 'Liberando endereço IP...\n'},
-             {'command': 'ipconfig /renew', 'description': 'Renovando endereço IP...\n'},
-             {'command': 'ipconfig /flushdns', 'description': 'Limpando cache DNS...\n'},
-             {'command': 'netsh winsock reset', 'description': 'Resetando Winsock...\n'},
-           ]
+    async def run_fix_internet_errors_script(self, cb):
+        await asyncio.sleep(1)  # Simulate some processing time
+        cb("Iniciando correção de erros de internet...")
 
-           for cmd in commands:
-               cb(cmd['description'])
-               try:
-                   await asyncio.sleep(1)
-                   result = subprocess.run(cmd['command'], shell=True, capture_output=True, text=True)
-                   print(f"Comando: {cmd['command']}\nSaída: {result.stdout}\nErro: {result.stderr}")
-                   if result.returncode == 0:
-                       cb(f"Comando '{cmd['command']}' executado com sucesso.")
-                   else:
-                       cb(f"Erro ao executar '{cmd['command']}': {result.stderr}")
-               except Exception as e:
-                   cb(f"Exceção ao executar '{cmd['command']}': {e}")
-    
+        commands = [
+            {
+                "command": "ipconfig /release",
+                "description": "Liberando endereço IP...\n",
+            },
+            {"command": "ipconfig /renew", "description": "Renovando endereço IP...\n"},
+            {"command": "ipconfig /flushdns", "description": "Limpando cache DNS...\n"},
+            {"command": "netsh winsock reset", "description": "Resetando Winsock...\n"},
+        ]
+
+        for cmd in commands:
+            cb(cmd["description"])
+            try:
+                await asyncio.sleep(1)
+                result = subprocess.run(
+                    cmd["command"], shell=True, capture_output=True, text=True
+                )
+                print(
+                    f"Comando: {cmd['command']}\nSaída: {result.stdout}\nErro: {result.stderr}"
+                )
+                if result.returncode == 0:
+                    cb(f"Comando '{cmd['command']}' executado com sucesso.")
+                else:
+                    cb(f"Erro ao executar '{cmd['command']}': {result.stderr}")
+            except Exception as e:
+                cb(f"Exceção ao executar '{cmd['command']}': {e}")
 
     async def test_fn(self, cb):
         await asyncio.sleep(1)  # Simulate some async processing time
         cb("Teste de callback funcionando!")
 
-
         subprocess.run('echo "Teste de callback funcionando!"', shell=True)
-
-
-
-
